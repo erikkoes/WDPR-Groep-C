f2688f2828a64438e886756efc9c7de4f84d642b
Deagarys <37025496+Deagarys@users.noreply.github.com>
2022-01-20 01:52:56 +0100

    .

---
+++ src/Controllers/ReportMessageController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using database;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using src.Models;
+
+namespace src.Controllers
+{
+    public class ReportMessageController : Controller
+    {
+        private readonly Context _context;
+
+        public ReportMessageController(Context context)
+        {
+            _context = context;
+        }
+        public IActionResult index(){
+            return View();
+        }
+        public IActionResult Report(string user, string message){
+            ReportMessageModel a = new ReportMessageModel();
+            a.UserId = user;
+            a.Message = message;
+            _context.Reports.Add(a);
+            return View();
+        }
+    }    
+}
---
+++ src/Models/ReportMessageModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace src.Models{
+    public class ReportMessageModel{
+        [Key]
+        public string ReportId{get;set;}
+        [Required]
+        public string UserId{get;set;}
+        [Required]
+        public string Message{get;set;}
+
+    }
+}
